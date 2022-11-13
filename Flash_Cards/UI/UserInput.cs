1e9b0684f61e36d453c33efd4629c05d204152e3
Stuart Willows <SJWillows91@outlook.com>
2022-11-13 08:45:39 +0000

    Added Stack Menu input using Spectre Console.

---
+++ Flash_Cards/UI/UserInput.cs
@@ -0,0 +1,30 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flash_Cards.UI;
+internal static class UserInput
+{
+    public static string ShowStackMenu()
+    {
+        var rule = new Rule("[bold blue]Main Menu[/]");
+        AnsiConsole.Write( rule );
+
+        var menu = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Please select an option:")
+                .PageSize(5)
+                .AddChoices(new[]
+                {
+                    "Create a new stack",
+                    "View all stacks",
+                    "Delete a stack",
+                    "Exit"
+                }));
+        return menu;
+    }
+
+}
