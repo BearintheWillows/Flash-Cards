2eb82dc083465e2863691629fd0028a2f20fc574
Stuart Willows <SJWillows91@outlook.com>
2022-11-16 14:27:20 +0000

    Moved Stack Menu from StackController to MenuController

---
+++ Flash_Cards/Controllers/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using Flash_Cards.Data;
+using Flash_Cards.UI;
+using Flash_Cards.View;
+using Models;
+using Spectre.Console;
+
+namespace Flash_Cards.Controllers;
+internal class MenuController
+{
+
+
+    public void ManageStackMenu( FlashCardContext db )
+    {
+        var choice = UserInput.GetStackMenuInput();
+        switch ( choice.ToLower() )
+        {
+            case "create a new stack":
+                var ReturnedStack = UserInput.GetNewStackInput();
+                db.AddStack( ReturnedStack );
+                ManageStackMenu( db );
+                break;
+            case "view all stacks":
+                var stacks = db.GetAllStacks();
+                UserView.DisplayStacks( stacks );
+                ManageStackMenu( db );
+                break;
+            case "delete a stack":
+                var stack = UserInput.GetStackToDelete();
+                Stack stackChoice = StackController.GetStackById( stack, db );
+                UserView.DisplayStackToDelete( stackChoice );
+                if ( UserInput.ConfirmChoice() )
+                {
+                    db.DeleteStack( stackChoice.Id );
+                    var rule = new Rule($"[bold red] Record Deleted[/]");
+                    AnsiConsole.Write( rule );
+
+                    ManageStackMenu( db );
+                }
+                else
+                {
+                    var rule = new Rule("[bold red] Record NOT Deleted[/]");
+                    AnsiConsole.Write( rule );
+                    ManageStackMenu( db );
+                }
+                break;
+        }
+
+    }
+}
--- Flash_Cards/Controllers/StackController.cs
+++ Flash_Cards/Controllers/StackController.cs
@@ -1,61 +1,13 @@
 ﻿using Flash_Cards.Data;
-using Flash_Cards.UI;
-using Flash_Cards.View;
 using Models;
 using Spectre.Console;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flash_Cards.Controllers;
 internal class StackController
 {
-    public void GetMenuChoice(FlashCardContext db)
-    {
-        var choice = UserInput.GetStackMenuInput();
-        switch ( choice.ToLower() )
-        {
-            case "create a new stack":
-                var ReturnedStack = UserInput.GetNewStackInput();
-                db.AddStack( ReturnedStack );
-                GetMenuChoice( db );
-                break;
-            case "view all stacks":
-                var stacks = db.GetAllStacks();
-                UserView.DisplayStacks( stacks );
-                GetMenuChoice( db );
-                break;
-            case "delete a stack":
-                var stack = UserInput.GetStackToDelete();
-                Stack stackChoice = GetStackById( stack, db );
-                UserView.DisplayStackToDelete( stackChoice );
-                if (UserInput.ConfirmChoice())
-                {
-                    db.DeleteStack( stackChoice.Id );
-                    var rule = new Rule($"[bold red] Record Deleted[/]");
-                    AnsiConsole.Write( rule );
-               
-                    GetMenuChoice(db);
-                }
-                else
-                {
-                    var rule = new Rule("[bold red] Record NOT Deleted[/]");
-                    AnsiConsole.Write( rule );
-                    GetMenuChoice( db );
-                }
-               
-                
-               
 
-                break;
 
-        }
-
-    }
-    
-    public static Stack GetStackById( int id, FlashCardContext db)
+    public static Stack GetStackById( int id, FlashCardContext db )
     {
         Stack stack = db.GetStackById( id );
         if ( stack != null )
